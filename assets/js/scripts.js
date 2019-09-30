40dc3e56ad394cd45c224ff7caa248ef4394ac4d
Kasutan <contact@kasutan.pro>
2019-09-30 10:16:13 +0200

    Modaal options

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,12 @@ jQuery(function ($) {
 	$('document').ready(function () {
 		console.log('test script');
 		$('button').modaal({
-			content_source:'#formulaire'
+			accessible_title: 'Je dépose mon dossier',
+			content_source:'#formulaire',
+			close_text:' Fermer',
+			close_aria_label:'Fermer (appuyer sur ESC pour fermer)',
+			overlay_opacity : 0.6,
+			background: '#000',
 		});
 	});
 });
