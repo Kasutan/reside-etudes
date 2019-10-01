7d8eaba8b716fdcfcb5b1570f270e6d68700f323
Kasutan <contact@kasutan.pro>
2019-10-01 18:02:20 +0200

    Form in modaal popup

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,63 @@
 jQuery(function ($) {
 	$('document').ready(function () {
-		console.log('test script');
 		$('button').modaal({
 			accessible_title: 'Je dépose mon dossier',
 			content_source:'#formulaire',
-			close_text:' Fermer',
+			close_text:'Fermer',
 			close_aria_label:'Fermer (appuyer sur ESC pour fermer)',
-			overlay_opacity : 0.6,
+			overlay_opacity : 0.8,
 			background: '#000',
+			width:695,
 		});
+
+		var validate_form = 1;
+		var theForm= jQuery('#theForm');
+		var email = jQuery('#email');
+		var firstname = jQuery('#firstname');
+		var lastname = jQuery('#lastname');
+		var tel = jQuery('#tel');
+		var pcode = jQuery('#pcode');
+		var city = jQuery('#city');
+		var landing = jQuery('#landing');
+		var optin = jQuery('#optin');
+		var submit = jQuery('#submit');
+
+		theForm.submit(function(e){
+			console.log("submit");
+			e.preventDefault();
+		});
+
+		$(".form-group input").focus(function (e) { 
+			$(this).parent().addClass('focus');
+		});
+
+		$(".form-group input").keypress(function (e) { 
+			checkFormComplete();
+		});
+
+		$(".form-group input").focusout(function() {
+			if ($(this).val()=="") {
+				$(this).parent().removeClass('focus');
+				submit.addClass('disabled');
+			} else {
+				checkFormComplete(); /*autocomplete*/
+			}
+		});
+
+		function checkFormComplete() {
+			var formComplete=1;
+			$(".form-group input").each(function () {
+				if ($(this).val()=="") {
+					formComplete=0;
+					return false;
+				} 
+			});
+			if (formComplete==1) {
+				submit.removeClass('disabled');
+			} else {
+				submit.addClass('disabled');
+			}
+		}
+
 	});
 });
